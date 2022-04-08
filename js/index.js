5ec0094349e0891f795d6864398461f26c010b26
Pablo Parise <pabloeparise@gmail.com>
2022-04-08 19:21:52 -0300

    changes

--- js/index.js
+++ js/index.js
@@ -1,8 +1,27 @@
-const $toggleButton=document.getElementById("menuToggle")
-const $NavList=document.getElementById("navbarList")
+/*-------------
+ Custom JS File
+ ------------*/
+
+// Menu displaying on small devices
+const $toggleButton = document.getElementById("menuToggle")
+const $navList = document.getElementById("navbarList")
 
 $toggleButton.addEventListener("click", handleMenu)
 
 function handleMenu() { 
-    $NavList.classList.toggle("active");
+    $navList.classList.toggle("active");
  }
+
+//Navbar style while scrolling 
+
+const $navbarLogo = document.getElementById("navbarLogo")
+const $navbarContainer = document.getElementById("navbarContainer")
+const $navbarPosition = $navbarContainer.getBoundingClientRect() //method that provides the element's position and its relative position to the viewport.
+
+function navbarTop () {
+    if ($navbarPosition.top === 0) {
+        $navbarLogo.classList.add("navbarSticked")
+    };
+}
+
+document.addEventListener('scroll', navbarTop)
