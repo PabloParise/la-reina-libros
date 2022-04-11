17c609fbb386f20fc430fb6ab6277739b180a6f5
Pablo Parise <pabloeparise@gmail.com>
2022-04-11 15:51:37 -0300

    navbar fixing

--- js/index.js
+++ js/index.js
@@ -4,26 +4,16 @@
 
 // Menu displaying on small devices
 const $toggleButton = document.getElementById("menuToggle")
-const $navList = document.getElementById("navbarList")
+const $navbarList = document.getElementById("navbarList")
 
 $toggleButton.addEventListener("click", handleMenu)
 
 function handleMenu() { 
-    $navList.classList.toggle("active");
+    $navbarList.classList.toggle("navbarActive");
  }
 
+ 
 //Navbar style while scrolling
-/*
-$(function() { 
-    $(window).scroll(function() {
-        if ($(".navbarContainer").position().top > 186) {  //186 is the navbar absolute position from top of the window 
-            $(".navbarLogo").addClass("navbarSticked");
-        } else {
-            $(".navbarLogo").removeClass("navbarSticked");
-        }
-    });
-});*/
-
 window.onscroll = function() {stickyStyle()};
 
 const $navbarContainer = document.getElementById("navbarContainer");
